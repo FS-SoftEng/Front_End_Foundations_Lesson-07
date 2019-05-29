782ea04f948c3d08fdbb5583a1c32c5c53183a89
Ian Ditsch <IanDitsch@Outlook.com>
2019-05-29 13:40:01 -0500

    FEF_Lesson 07

---
+++ script.js
@@ -0,0 +1,12 @@
+function phoneNumberFormat() {
+    let phoneNumber = document.getElementById('phNumber').value;
+    let phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/;
+    if (phoneNumber.match(phoneRegex)) {
+      alert('Valid number entered');
+      console.log(true);
+    }
+    else {
+      alert('Phone number entered is invalid');
+      console.log(false);
+    }
+  }
